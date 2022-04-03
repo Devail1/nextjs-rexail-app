22e9f640d1b06a692ead6128fa4a0ae978d8f9d5
Liav Edry <liav.acc@gmail.com>
2022-04-03 23:50:24 +0300

    Capitalize component name

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,13 @@
-import HeaderMenu from 'components/HeaderMenu'
-import FooterMenu from 'components/FooterMenu'
-import {ReactNode} from "react";
+import HeaderMenu from "components/HeaderMenu";
+import FooterMenu from "components/FooterMenu";
+import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
-    return (
-        <div className="bg">
-            <HeaderMenu />
-            <main>{children}</main>
-            <FooterMenu />
-        </div>
-    )
+  return (
+    <div className="bg">
+      <HeaderMenu />
+      <main>{children}</main>
+      <FooterMenu />
+    </div>
+  );
 }
