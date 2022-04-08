24815f9dfe97d39b49f8bce048f27218a00fd72f
Liav Edry <liav.acc@gmail.com>
2022-04-09 00:39:10 +0300

    wip

--- src/components/storeItem.tsx
+++ src/components/storeItem.tsx
@@ -1,5 +1,4 @@
-import { TCartActions, TCartState, useCartState } from "hooks/useCartState";
-import React from "react";
+import { TCartActions } from "hooks/useCartState";
 import { TProduct, Unit } from "types";
 
 interface Props {
@@ -223,18 +222,4 @@ const StoreItem = ({ product, currencySign, cartActions }: Props) => {
   );
 };
 
-function areEqual(prevProps: Props, nextProps: Props) {
-  if (prevProps.product) console.log("prevProps", prevProps.product);
-  if (nextProps.product) console.log("nextProps", nextProps.product);
-
-  // console.log("prevProps", prevProps.cartState.cartItems.length);
-  // console.log("nextProps", nextProps.cartState.cartItems.length);
-
-  // return (
-  //   // prevProps.product.quantity === nextProps.product.quantity &&
-  //   // prevProps.cartState.cartItems.length === nextProps.cartState.cartItems.length
-  // );
-  return false;
-}
-
-export default React.memo(StoreItem, areEqual);
+export default StoreItem;
